426e3e0d893c8f4047c7630ac97efa68b3884587
Karl Larson <KarlLarson@Karls-MacBook-Air.local>
2014-11-26 12:00:24 -0700

    Wednesday Noon
    Before fixing home js height

--- wp-content/themes/velowood/templates/products.php
+++ wp-content/themes/velowood/templates/products.php
@@ -17,58 +17,79 @@ get_header(); ?>
 				<?php get_template_part( 'content', 'page' ); ?>
 
 				<!-- START BICYCLES PRODUCTS SECTION -->
+			<div class="container-fluid">
+				
+			</div>
+					<!-- START OF BIKE MAKERS -->		
+			
+			<div class="parts" style="background: url(<?php the_field('products_parts_background'); ?>)">
 			<div class="container">
+				<div class="row">
 					<div class="col-xs-12 center">
 						<h1><?php the_field('products_bicycles_header'); ?></h1>
 					</div>
-				
-					<!-- START OF BIKE MAKERS -->
-				
-				<div class="row">
-					<div class="col-xs-12 col-sm-6 col-md-3">
-					<div class="center">
-						<h3><?php the_field('products_bicycles_subheader_1'); ?></h3>
-					</div>
-					<img class="bike_maker" src="<?php the_field('products_bicycles_image_1'); ?>" >
-					<p><?php the_field('products_bicycles_text_1'); ?></p>
-					</div>
-
-					<div class="col-xs-12 col-sm-6 col-md-3">
-					<div class="center">
-						<h3><?php the_field('products_bicycles_subheader_2'); ?></h3>
-					</div>
-					<img class="bike_maker" src="<?php the_field('products_bicycles_image_2'); ?>" >
-					<p><?php the_field('products_bicycles_text_2'); ?></p>
-					</div>
-
-					<div class="col-xs-12 col-sm-6 col-md-3">
-					<div class="center">
-						<h3><?php the_field('products_bicycles_subheader_3'); ?></h3>
-					</div>
-					<img class="bike_maker" src="<?php the_field('products_bicycles_image_3'); ?>" >
-					<p><?php the_field('products_bicycles_text_3'); ?></p>
-					</div>
-
-					<div class="col-xs-12 col-sm-6 col-md-3">
-					<div class="center">
-						<h3><?php the_field('products_bicycles_subheader_4'); ?></h3>
-					</div>
-					<img class="bike_maker" src="<?php the_field('products_bicycles_image_4'); ?>" >
-					<p><?php the_field('products_bicycles_text_4'); ?></p>
-					</div>
-				</div>
-				</div>
-			<div class="container-fluid">
-				<div class="parts" style="background: url(<?php the_field('products_parts_background'); ?>)">
-				<div class="row">
 					<div class="col-xs-12 col-sm-4 col-sm-offset-1">
 						<div class="clear_text">
 						<p><?php the_field('products_parts_text'); ?></p>
 						</div>
 					</div>
 				</div>
+			</div>
+			</div>
+
+			<div class="bikes">
+			<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-3">
+				<div class="center">
+					<h3><?php the_field('products_bicycles_subheader_1'); ?></h3>
+				</div>
+				<img class="bike_maker" src="<?php the_field('products_bicycles_image_1'); ?>" >
+				<p><?php the_field('products_bicycles_text_1'); ?></p>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-3">
+				<div class="center">
+					<h3><?php the_field('products_bicycles_subheader_2'); ?></h3>
+				</div>
+				<img class="bike_maker" src="<?php the_field('products_bicycles_image_2'); ?>" >
+				<p><?php the_field('products_bicycles_text_2'); ?></p>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-3">
+				<div class="center">
+					<h3><?php the_field('products_bicycles_subheader_3'); ?></h3>
+				</div>
+				<img class="bike_maker" src="<?php the_field('products_bicycles_image_3'); ?>" >
+				<p><?php the_field('products_bicycles_text_3'); ?></p>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-3">
+				<div class="center">
+					<h3><?php the_field('products_bicycles_subheader_4'); ?></h3>
+				</div>
+				<img class="bike_maker" src="<?php the_field('products_bicycles_image_4'); ?>" >
+				<p><?php the_field('products_bicycles_text_4'); ?></p>
 				</div>
 			</div>
+			</div>
+			</div>
+
+			<div class="accessories">
+			<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2 center">
+				<h3><?php the_field('accessories_title'); ?></h3>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2">
+				<p><?php the_field('accessories_text'); ?></p>
+				</div>
+			</div>
+			</div>
+			</div>
+			
 				<!-- END OF BIKE MAKERS -->
 				<!-- START OF ACCESORIES -->
 			<?php endwhile; // end of the loop. ?>
--- wp-content/themes/velowood/templates/services.php
+++ wp-content/themes/velowood/templates/services.php
@@ -23,36 +23,111 @@ get_header(); ?>
 			<h2><?php the_field ('services_header'); ?></h2>
 		</div>
 	</div>
-
 	<div class="row">
 		<div class="col-xs-12 center">
 		<h5><?php the_field('services_subheader'); ?></h5>
 		</div>
 	</div>
-
-	<div class="row">
-	<div class="col-xs-12 col-sm-4">
-		<img class="service_image" src="<?php the_field('services_image_1'); ?>" >
-		<p><?php the_field('services_text_1'); ?></p>
-	</div>
-
-	<div class="col-xs-12 col-sm-4">
-		<img class="service_image" src="<?php the_field('services_image_2'); ?>" >
-		<p><?php the_field('services_text_2'); ?></p>
-	</div>
-
-	<div class="col-xs-12 col-sm-4">
-		<img class="service_image" src="<?php the_field('services_image_3'); ?>" >
-		<p><?php the_field('services_text_3'); ?></p>
-	</div>
-	</div>
 	</div>
 
 	<div class="tuneup">
-		
+	<div class="container-fluid">
+	<div class="row">
+	<div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 center">
+	<h3><?php the_field('services_tune_1'); ?></h3>
+	<div class="tuneup_box">
+
+	<!-- START REPEATER -->
+	<?php
+	// check if the repeater field has rows of data
+	if( have_rows('bike_check') ):
+
+	 	// loop through the rows of data
+	    while ( have_rows('bike_check') ) : the_row(); ?>
+
+	        <!-- // display a sub field value -->
+	        <div class="row">
+	        <?php the_sub_field('bike_check_feature'); ?>
+			</div>
+	    <?php endwhile;
+
+	else :
+
+	    // no rows found
+
+	endif;
+
+	?>
+	<!-- END REPEATER -->
+	</div>
+	</div>
+	<div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 center">
+	<h3><?php the_field('services_tune_2'); ?></h3>
+	<div class="tuneup_box">
+	<!-- START REPEATER -->
+	<?php
+	// check if the repeater field has rows of data
+	if( have_rows('basic_tune') ):
+
+	 	// loop through the rows of data
+	    while ( have_rows('basic_tune') ) : the_row(); ?>
+
+	        <!-- // display a sub field value -->
+	        <div class="row">
+	        <?php the_sub_field('basic_tune_feature'); ?>
+			</div>
+	    <?php endwhile;
+
+	else :
+
+	    // no rows found
+
+	endif;
+
+	?>
+	<!-- END REPEATER -->
+	</div>
+	</div>
+	<div class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 center">
+	<h3><?php the_field('services_tune_3'); ?></h3>
+	<div class="tuneup_box">
+	<!-- START REPEATER -->
+	<?php
+	// check if the repeater field has rows of data
+	if( have_rows('comp_tune') ):
+
+	 	// loop through the rows of data
+	    while ( have_rows('comp_tune') ) : the_row(); ?>
+
+	        <!-- // display a sub field value -->
+	        <div class="row">
+	        <?php the_sub_field('comp_tune_feature'); ?>
+			</div>
+	    <?php endwhile;
+
+	else :
+
+	    // no rows found
+
+	endif;
+
+	?>
+	<!-- END REPEATER -->
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	<!-- END OF TUNEUP SECTION -->
+	<!-- START OF SERVICES EXPLAINED -->
+	<div class="services_area">
+	<div class="container_fluid">
+	<div class="row">
+	
+	</div>
+	</div>
 	</div>
 
-	
 				<!-- END OF SERVICES		 -->
 
 			<?php endwhile; // end of the loop. ?>
