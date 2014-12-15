859eef8dacf11cca7d2a68f73156b3998edad595
Karl Larson <KarlLarson@Karls-Air.lan>
2014-12-15 09:51:52 -0700

    Monday Morning
    After changing Products to full line per manufacturer

--- wp-content/themes/velowood/templates/products.php
+++ wp-content/themes/velowood/templates/products.php
@@ -38,41 +38,56 @@ get_header(); ?>
 			</div>
 			<!-- START OF BIKE MAKERS SECTION -->
 			<div class="bikes">
-			<div class="container-fluid">
+			<div class="container">
 			<div class="row">
-				<div class="col-xs-12 col-sm-6 col-md-3">
+				<div class="col-xs-12 col-sm-4">
 				<div class="center">
 					<h3><?php the_field('products_bicycles_subheader_1'); ?></h3>
 				</div>
-				<img class="bike_maker" src="<?php the_field('products_bicycles_image_1'); ?>" >
 				<p><?php the_field('products_bicycles_text_1'); ?></p>
 				</div>
+				<div class="col-xs-12 col-sm-8">
+				<img class="bike_maker" src="<?php the_field('products_bicycles_image_1'); ?>" >
+				</div>
+			</div>
 
-				<div class="col-xs-12 col-sm-6 col-md-3">
+			<div class="row">
+				<div class="col-xs-12 col-sm-8">
+				<img class="bike_maker" src="<?php the_field('products_bicycles_image_2'); ?>" >
+				</div>
+				<div class="col-xs-12 col-sm-4">
 				<div class="center">
 					<h3><?php the_field('products_bicycles_subheader_2'); ?></h3>
 				</div>
-				<img class="bike_maker" src="<?php the_field('products_bicycles_image_2'); ?>" >
 				<p><?php the_field('products_bicycles_text_2'); ?></p>
 				</div>
+			</div>
 
-				<div class="col-xs-12 col-sm-6 col-md-3">
+			<div class="row">
+				<div class="col-xs-12 col-sm-4">
 				<div class="center">
 					<h3><?php the_field('products_bicycles_subheader_3'); ?></h3>
 				</div>
-				<img class="bike_maker" src="<?php the_field('products_bicycles_image_3'); ?>" >
 				<p><?php the_field('products_bicycles_text_3'); ?></p>
 				</div>
+				<div class="col-xs-12 col-sm-8">
+				<img class="bike_maker" src="<?php the_field('products_bicycles_image_3'); ?>" >
+				</div>
+			</div>
 
-				<div class="col-xs-12 col-sm-6 col-md-3">
+			<div class="row">
+				<div class="col-xs-12 col-sm-8">
+				<img class="bike_maker" src="<?php the_field('products_bicycles_image_4'); ?>" >
+				</div>
+				<div class="col-xs-12 col-sm-4">
 				<div class="center">
 					<h3><?php the_field('products_bicycles_subheader_4'); ?></h3>
 				</div>
-				<img class="bike_maker" src="<?php the_field('products_bicycles_image_4'); ?>" >
 				<p><?php the_field('products_bicycles_text_4'); ?></p>
 				</div>
 			</div>
-			</div>
+
+			</div>					
 			</div>
 			<!-- END OF BIKE MAKERS SECTION -->
 			<!-- PAGE BREAK -->
